96ebba41197879e456776473fe12df7b94508bed
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-08-08 16:45:14 -0500

    finished mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,36 +2,69 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-// axios.get('https://api.github.com/users/MSquared88')
-//   .then((response) => {
-//     console.log(response)
-//   })
+axios.get('https://api.github.com/users/MSquared88')
+  .then((response) => {
+    const matthew = newGitUser(response)
+    cards.appendChild(matthew)
+    console.log(response)
+
+  })
+  // .then((res) )
+const cards = document.querySelector('.cards')
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+github info! You will need to understand the structure of this 
+data in order to use it to build your component function 
 
-   Skip to Step 3.
+Skip to Step 3.
 */
 
 /* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
+create a new component and add it to the DOM as a child of .cards
 */
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
+follow this link in your browser https://api.github.com/users/<Your github name>/followers 
+, manually find some other users' github handles, or use the list found 
+at the bottom of the page. Get at least 5 different Github usernames and add them as
+Individual strings to the friendsArray below.
+
+Using that array, iterate over it, requesting data for each user, creating a new card for each
+user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  'https://api.github.com/users/MWeberLambdaweb19',
+  'https://api.github.com/users/JaxAtwood', 
+  'https://api.github.com/users/chrisbonifacio',
+  'https://api.github.com/users/yoshimii',
+  'https://api.github.com/users/ajflowers',
+  'https://api.github.com/users/MosesSupposes',
+  'https://api.github.com/users/briannakeune'
+];
+
+followersArray.forEach((item) => {
+  axios.get(item)
+    .then((response) => {
+      let follower  = newGitUser(response)
+      cards.appendChild(follower)
+      
+    })
+})
+
+
+  // axios.get(res)
+  //   .then((res) => {
+
+  //   })
+  //   .then((response) => {
+  //     let follower  = newGitUser(response)
+  //     cards.appendChild(follower)
+      
+  //   })
 
 /* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+Using DOM methods and properties, create a component that will return the following DOM element:
 
 <div class="card">
   <img src={image url of user} />
@@ -51,15 +84,64 @@ const followersArray = [];
 */
 
 function newGitUser(obj){
+  //nested iside of cards div
   const newUser = document.createElement('div')
   newUser.classList.add('card')
 
+  //nested inside of newUser
   const userImg = document.createElement('img')
+  userImg.src = obj.data.avatar_url
 
+  //nested inside of newUser
   const userInfo = document.createElement('div')
   userInfo.classList.add('card-info')
 
+  //nested inside of userInfo
+  const userName = document.createElement('h3')
+  userName.classList.add('name')
+  userName.textContent = obj.data.name
 
+  //nested inside of userInfo
+  const userHandle = document.createElement('p')
+  userHandle.classList.add('username')
+  userHandle.textContent = obj.data.login
+  
+  //nested inside of userInfo
+  const userProfile = document.createElement('p')
+
+  //nested inside of userProfile
+  const userProfileLink = document.createElement('a')
+  userProfileLink.href = obj.data.html_url
+  userProfileLink.textContent = obj.data.html_url
+  
+  //nested inside of newUser
+  const userFollowers = document.createElement('p')
+  userFollowers.textContent = `Followers: ${obj.data.followers} `
+  
+  //nested inside of newUser
+  const userFollowing = document.createElement('p')
+  userFollowing.textContent = `Following: ${obj.data.following} `
+
+  //nested inside of newUser
+  const userBio = document.createElement('p')
+  userBio.textContent = obj.data.bio
+
+  newUser.appendChild(userImg)
+
+  newUser.appendChild(userInfo)
+  userInfo.appendChild(userName)
+  userInfo.appendChild(userHandle)
+
+  userInfo.appendChild(userProfile)
+  userProfile.appendChild(userProfileLink)
+
+  userInfo.appendChild(userFollowers)
+  userInfo.appendChild(userFollowing)
+  userInfo.appendChild(userBio)
+
+  
+
+  return newUser
 }
 
 
